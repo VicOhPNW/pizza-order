5675589899805a9b21a4bf4ff4cce0ad8afda3d6
Victoria Oh <ohvictori@gmail.com>
2018-08-28 19:43:28 -0700

    add additional styling

--- js/scripts.js
+++ js/scripts.js
@@ -41,53 +41,41 @@ PizzaOrder.prototype.amount = function() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-// //Frontend Logic
-// $(document).ready(function() {
-//   $("#pizzaForm").submit(function(event) {
-//     event.preventDefault();
+// Original code that wasn not working:
+// // Frontend Logic
+//     $(document).ready(function() {
+//       $("#pizzaForm").submit(function(event) {
+//         event.preventDefault();
 //
-//     var size = $("input:radio[name=size]:checked").val();
-//     var topping = $("input:checkbox[name=topping]:checked").val();
-//     var fulfillment = $("input:radio[name=fulfillment]:checked").val();
+//         var size = $("input:radio[name=size]:checked").val();
+//         var topping = $("input:checkbox[name=topping]:checked").val();
+//         var fulfillment = $("input:radio[name=fulfillment]:checked").val();
 //
-//     var newPizzaOrder = new PizzaOrder(size, topping, fulfillment);
+//         var newPizzaOrder = new PizzaOrder(size, topping, fulfillment);
 //
-//     $("#pizzaTotal").text(" $" + newPizzaOrder.amount());
-//   });
-// });
+//         $("#pizzaTotal").text(" $" + newPizzaOrder.amount().toppings());
+//       });
+//     });
 //
 //
 //
-// //Backend Logic
-// function PizzaOrder(size, toppings, fulfillment) {
-//   this.size = size;
-//   this.toppings = toppings;
-//   this.fulfillment = fulfillment;
-// }
+// // Backend Logic
+//     function PizzaOrder(size, toppings, fulfillment) {
+//       this.size = size;
+//       this.toppings = toppings;
+//       this.fulfillment = fulfillment;
+//     }
 //
-// var pizzaSize = {"small":6, "medium":8, "large":10};
-// var pizzaTopping = {"olives":1, "mushrooms":1, "onion":1};
-// var pizzaFulfillment = {"delivery":3, "pickup":0};
+//     var pizzaSize = {"small":6, "medium":8, "large":10};
+//     var pizzaTopping = {"olives":1, "mushrooms":1, "onion":1};
+//     var pizzaFulfillment = {"delivery":3, "pickup":0};
 //
-// PizzaOrder.prototype.amount = function() {
-//   return pizzaSize[this.size] + pizzaTopping[this.topping] + pizzaFulfillment[this.fulfillment];
-// };
+//     PizzaOrder.prototype.amount = function() {
+//       return pizzaSize[this.size] + pizzaTopping[this.topping] + pizzaFulfillment[this.fulfillment];
+//     };
 //
-// PizzaOrder.prototype.toppings = function() {
-//  var toppingTotal = 0;
-//  for (var i = 0; i < this.toppings.length; i++) {
-//    toppingTotal += pizzaTopping[this.toppings[i].value];
-//
-//  }
-//  return pizzaSize[this.size] + toppingTotal + pizzaFulfillment[this.fulfillment];
-// };
+//     PizzaOrder.prototype.toppings = function() {
+//      var toppingTotal = 0;
+//      for (var i = 0; i < this.toppings.length; i++) {
+//        toppingTotal += pizzaTopping[this.toppings[i].value];
+//     };
